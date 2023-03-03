b9c4dbe875e149214fa704e358871575b8677ddc
jani-heinikoski <59805433+jani-heinikoski@users.noreply.github.com>
2023-03-03 16:27:19 +0200

    fixed indentations

--- int_arithmetic.s
+++ int_arithmetic.s
@@ -25,48 +25,48 @@ main:
 	movq	%rsp, %rbp
 
 loop:
-	# ----- print x: ----- 
-	# First argument is given to rdi which is the printf's format
-	movq	$printf_string_format, %rdi
-	# Second argument is given to rsi which is the printf's message
-	movq	$message, %rsi
-	# Set rax to 0 because we don't give printf any floating point varargs
-	movq	$0, %rax
-	# Call the printf function
-	call 	printf
+    # ----- print x: ----- 
+    # First argument is given to rdi which is the printf's format
+    movq	$printf_string_format, %rdi
+    # Second argument is given to rsi which is the printf's message
+    movq	$message, %rsi
+    # Set rax to 0 because we don't give printf any floating point varargs
+    movq	$0, %rax
+    # Call the printf function
+    call 	printf
 
     # IMPLEMENT THE CALCULATIONS HERE, YOU CAN USE THE PRINTING TO CHECK IF IT IS DONE CORRECTLY
 
     # Signed integer division
     # idivq divides %rdx:%rax with r64/m64. Quotient --> rax, remainder --> rdx
     # Zero out the register rdx so that signed division works properly
-	xor     %rdx, %rdx
-	movq    N, %rax
-	idivq	x
+    xor     %rdx, %rdx
+    movq    N, %rax
+    idivq	x
     # Save the quotient to x
     movq    %rax, x
-    
 
-	# ----- print the value of x ----- 
-	# First argument is given to rdi which is the printf's format
-	movq	$printf_int_format, %rdi
-	# Give the new value of x to printf as the second parameter
-	movq	x, %rsi
-	# Set rax to 0 because we give printf zero floating point varargs
-	movq	$0, %rax
-	# Call the printf function
-	call 	printf
 
-	# Jump back too loop label if i != 0
-	movq	i, %rax
-	subq	$1, %rax
-	movq	%rax, i
-	cmpq 	$0, i
-	jnz 	loop
+    # ----- print the value of x ----- 
+    # First argument is given to rdi which is the printf's format
+    movq	$printf_int_format, %rdi
+    # Give the new value of x to printf as the second parameter
+    movq	x, %rsi
+    # Set rax to 0 because we give printf zero floating point varargs
+    movq	$0, %rax
+    # Call the printf function
+    call 	printf
 
-	# Restore the previously saved stack frame and return zero (OK) 
-	leave
-	ret	$0
+    # Jump back too loop label if i != 0
+    movq	i, %rax
+    subq	$1, %rax
+    movq	%rax, i
+    cmpq 	$0, i
+    jnz 	loop
+
+    # Restore the previously saved stack frame and return zero (OK) 
+    leave
+    ret	$0
 
 # Data section for defining labels
 .data
